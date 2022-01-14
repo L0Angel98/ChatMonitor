e9175f7421ab460597a8c9c2aee0e7502b1ec3fc
Luis Ángel Velázquez Palomino <angelvelazquez98@hotmail.com>
2022-01-14 09:55:02 -0600

    add hot server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,14 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json({ message: "Hola esta es la pagina principal hola!" });
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
