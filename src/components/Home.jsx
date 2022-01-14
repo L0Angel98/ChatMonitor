0adcc3b58b72330399a5019ccb8f55c7f4b59dda
Luis Ángel Velázquez Palomino <angelvelazquez98@hotmail.com>
2022-01-14 13:56:19 -0600

    send data chat

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import socketIOClient from "socket.io-client";
+import "./Home.css";
 const ENDPOINT = "http://localhost:3001";
 
 const testFetch = async () => {
@@ -13,35 +14,69 @@ const testFetch = async () => {
 };
 
 export default function Home() {
-  const [resp, setResp] = useState("");
+  const [resp, setResp] = useState([]);
+  const [dataSend, setDataSend] = useState({name: "", message: ""})
 
-  const handleClick = async () => {
-    try {
-      const resp = await axios.post("http://localhost:3001/message", {
-        message: "Hola mundo",
-        name: "getsemani"
-      });
-      // console.log(resp);
-    } catch (error) {}
+  const handleClick = async event => {
+    event.preventDefault();
+
+    if(!!dataSend.name && !!dataSend.message){
+      try {
+        const resp = await axios.post("http://localhost:3001/message", dataSend);
+        // console.log(resp);
+      } catch (error) {}
+    }
+
+    
   };
 
   useEffect(() => {
     testFetch();
   }, []);
 
+  const onChange = event => {
+    const { name, messange } = event.target;
+    setDataSend({...dataSend, [name]: messange})
+  };
+
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT, {
-      withCredentials: true
+      withCredentials: true,
     });
-    socket.on("message_12", data => {
+    socket.on("message_12", (data) => {
       console.log("mensaje", data);
-      setResp(data);
+      setResp([...data]);
     });
   }, []);
 
   return (
-    <div>
-      <button onClick={handleClick}>Send Message</button>Respuesta: {resp.name}
+    <div className="mainContainer">
+      <div className="header">Monitor Chat</div>
+      <div className="body">
+      <form onSubmit={handleClick} className="formStyles">
+        <label>
+          Nombre
+          <input name="name" type="text" value={dataSend.name} onChange={onChange} required />
+        </label>
+        <label>
+          Mensaje
+          <input name="message" type="text" value={dataSend.message} onChange={onChange} required/>
+        </label>
+        <button type="submit" value="Submit" >Send Message</button>
+      </form>
+      <div className="messange">
+        {resp.length > 0 ? (
+          resp.map((item) => (
+            <div>
+              {item.name} <label>{item.messange}</label>
+            </div>
+          ))
+        ) : (
+          <div>No hay mensajes aún</div>
+        )}
+      </div>
+      </div>
+      
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,10 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.(c|sc|sa)ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       }
     ]
   }
