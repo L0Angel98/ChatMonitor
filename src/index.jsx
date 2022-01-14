7153a2d426a2f11b7784255e2d902baa93c8c1b2
giie30 <getse@parrotlabs.com.mx>
2022-01-14 09:58:59 -0600

    config webapack and server 9000

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  React.createElement(
+    "div",
+    null,
+    `Complete Guide to Webpack Configuration for React`
+  ),
+  document.getElementById("app")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,47 @@
 const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, "src", "index.js"),
+  entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
+  },
+  devServer: {
+    // contentBase
+    static: {
+      directory: path.join(__dirname, "public/")
+    },
+    port: 9000,
+    // publicPath
+    devMiddleware: {
+      publicPath: "https://localhost:3000/dist/"
+    },
+    // hotOnly
+    hot: "only"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    targets: "defaults"
+                  }
+                ],
+                "@babel/preset-react"
+              ]
+            }
+          }
+        ]
+      }
+    ]
   }
 };
