92a5f84afe3e4653fbe34ed237b42e6a677bc03d
Monitor Developer <monitor@iMac-de-Monitor.local>
2022-01-14 11:09:50 -0600

    add sockets

--- server/index.js
+++ server/index.js
@@ -1,17 +1,36 @@
 const express = require("express");
-
-const PORT = process.env.PORT || 3001;
-
 const app = express();
+app.use(express.static(__dirname));
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+const port = process.env.PORT || 3001;
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const messages = [
+  {name: "Yo",message:"Hello"},
+  {name: "Pam",message:"hi"}
+]
+io.on('connection', () => {
+  console.log('a user connected');
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "Hola esta es la pagina principal hola!" });
 });
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.get('/messages', (req, res) => {
+  res.send(messages);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+app.post('/message', (req, res) => {
+  messages.push(req.body);
+  io.emit('message', req.body);
+  console.log('Connected clients');
+  res.sendStatus(200);
+});
+
+const server = app.listen(port, () => {
+ const { port } = server.address();
+ console.log(`Listening on port ${port}`);
 });
